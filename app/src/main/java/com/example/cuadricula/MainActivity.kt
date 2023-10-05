202e693cecb1363c094d532f87bfa90e006fa18f
Esteban Alvarez Nuñez <s19120154@gmail.com>
2023-10-05 15:56:58 -0600

    Seguimos......

--- app/src/main/java/com/example/cuadricula/MainActivity.kt
+++ app/src/main/java/com/example/cuadricula/MainActivity.kt
@@ -99,3 +99,18 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @Preview(showBackground = true)
+    @Composable
+    fun TopicPreview() {
+        CuadriculaTheme {
+            val topic = Topic(R.string.photography, 321, R.drawable.photography)
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TopicCard(topic = topic)
+            }
+        }
+    }
+}
