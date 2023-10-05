5d170f53ab0b369badc1fac5e2aaad61c91b768f
Esteban Alvarez Nuñez <s19120154@gmail.com>
2023-10-05 14:13:37 -0600

    Initial commit

---
+++ app/src/main/java/com/example/cuadricula/DataSource.kt
@@ -0,0 +1,11 @@
+package com.example.cuadricula
+
+
+object DataSource {
+
+    val topics = listOf(
+        Topic(R.string.architecture, 58, R.drawable.architecture),
+        Topic(R.string.crafts, 121, R.drawable.crafts),
+
+    )
+}
---
+++ app/src/main/java/com/example/cuadricula/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.cuadricula
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.cuadricula.ui.theme.CuadriculaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CuadriculaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TopicGrid()
+                }
+            }
+        }
+    }
+
+
+
